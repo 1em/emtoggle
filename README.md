c86b9dd2557e3c1727778b7c7ddfea4d5278d8e5
AK <github1em@gmail.com>
2013-09-27 16:34:21 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,25 @@
+# emToggle, a minimalist Javascript and CSS toggle for HTML Elements
+
+# How it works:
+
+1. Plug in the CSS and Javascript files to the HTML page you are working on.
+2. Three data properties are used to toggle elements:
+    
+    data-emtoggle
+    data-emlink
+    data-emvisible
+
+3. On the clickable or touchable toggle element, add the HTML code as follows:
+    
+    <button data-emlink="somemenu" data-emtoggle>Menu</button>
+
+4. On the invisible menu element, add the following HTML code:
+    <div data-emlink="somemenu">Menu things here.</div>
+
+## Notes: 
+
+On initial window load, all non data-emtoggle elements with data-emlink values will be invisible (CSS display: none)
+
+You can have more than two elements with the same data-emlink values. They will all toggle with their data-emtoggle element(s).
+
+Also, classes or ids are separate from the data-emlink values. This, hopefully, will separate CSS styling from toggle functions.
---
+++ emtoggle.js
@@ -0,0 +1,16 @@
+function toggleListener(event) {
+    var data_link = event.target.getAttribute("data-emlink") // get the clicked element's data-emlink value
+    visibles = document.querySelectorAll("[data-emlink=" + data_link + "]")     
+    var state_string = "false" 
+    if (event.target.getAttribute("data-emvisible") == "false") state_string = "true"  
+    for (var i=0; i < visibles.length; i++) visibles[i].setAttribute("data-emvisible", state_string)
+}
+        
+window.onload = function initialize() {
+    var toggle_elements = document.querySelectorAll("[data-emtogggle]"); // all data-emtogggles
+    var visibles = document.querySelectorAll("[data-emlink]"); // all data-emlinks        
+    var string_type = "click" // default is click
+    if (('ontouchstart' in window) || ('msmaxtouchpoints' in window.navigator)) string_type = "touchend"
+    for (var i=0; i < toggle_elements.length; i++) toggle_elements[i].addEventListener(string_type, toggleListener, true) 
+    for (var i=0; i < visibles.length; i++) visibles[i].setAttribute("data-emvisible", "false") //initialize to false
+}
