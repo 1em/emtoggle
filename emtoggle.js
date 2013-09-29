0b91195bd74b09525efc01e6a4bd25ec3707aad1
AK <github1em@gmail.com>
2013-09-28 20:24:54 -0500

    Type in JS file, fixed

--- emtoggle.js
+++ emtoggle.js
@@ -7,7 +7,7 @@ function toggleListener(event) {
 }
         
 window.onload = function initialize() {
-    var toggle_elements = document.querySelectorAll("[data-emtogggle]"); // all data-emtogggles
+    var toggle_elements = document.querySelectorAll("[data-emtoggle]"); // all data-emtoggles
     var visibles = document.querySelectorAll("[data-emlink]"); // all data-emlinks        
     var string_type = "click" // default is click
     if (('ontouchstart' in window) || ('msmaxtouchpoints' in window.navigator)) string_type = "touchend"
